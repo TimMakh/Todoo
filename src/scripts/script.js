e36238f4d92961c22ef778996e7d7bf40f1e77c4
TimMakh <ti-nb654321@mail.ru>
2023-06-08 23:28:10 +0500

    Lesson5

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,5 @@
 const title = document.querySelector(".todo__input");
 
-const renderTodo = (data) => {
-  const list = document.querySelector(".todo__list");
-  const template = document
-    .querySelector(".todo__template")
-    .content.querySelector(".todo__item");
-  const newItem = template.cloneNode(true);
-  newItem.querySelector(".todo__task").textContent = title.value;
-  list.appendChild(newItem);
-};
-
 const sendData = async (url, data) => {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
     method: "POST",
@@ -22,45 +12,62 @@ const sendData = async (url, data) => {
     },
   })
     .then((response) => response.json())
-    .then((json) => renderTodo(json));
+    .then((json) => renderItem(json));
 };
 
 const sendTodo = () => {
   const form = document.querySelector(".todo__form");
-
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-
     const Todo = JSON.stringify(title.value);
-
     sendData("https://jsonplaceholder.typicode.com/todos", Todo);
   });
 };
 
-const modalElem = document.querySelector(".modal");
-const btnCloseModal = document.querySelector(".modal__btn--close");
-
-btnCloseModal.addEventListener("click", function (closeModal) {
-  modalElem.style.visibility = "hidden";
-});
+const renderItem = (item) => {
+  const list = document.querySelector(".todo__list");
+  const template = document
+    .querySelector(".todo__template")
+    .content.querySelector(".todo__item");
+  const newItem = template.cloneNode(true);
+  newItem.querySelector(".todo__task").textContent = item.title;
+  newItem.querySelector(".todo__checkbox").addEventListener("change", (evt) => {
+    if (evt.target.checked) {
+      newItem.querySelector(".todo__task").style.textDecoration =
+        "line-through";
+    } else {
+      newItem.querySelector(".todo__task").style.textDecoration = "none";
+    }
+  });
+  newItem
+    .querySelector(".todo__btn-priority--high")
+    .addEventListener("click", (evt) => {
+      newItem.querySelector(".todo__task").style.color = "red";
+    });
+  newItem
+    .querySelector(".todo__btn-priority--normal")
+    .addEventListener("click", (evt) => {
+      newItem.querySelector(".todo__task").style.color = "green";
+    });
+  newItem
+    .querySelector(".todo__btn-priority--low")
+    .addEventListener("click", (evt) => {
+      newItem.querySelector(".todo__task").style.color = "yellow";
+    });
+  newItem.dataset.id = item.id;
+  newItem
+    .querySelector(".todo__btn-priority--remove")
+    .addEventListener("click", function (evt) {
+      const itemID = evt.target.closest(".todo__item").dataset.id;
+      this.dataset.id = itemID;
+      const removeTodo = evt.target.closest(".todo__item").remove();
+    });
+  newItem.querySelector(".todo__checkbox");
+  list.appendChild(newItem);
+};
 
 const renderList = (data) => {
-  const list = document.querySelector(".todo__list");
-  data.forEach((item) => {
-    const template = document
-      .querySelector(".todo__template")
-      .content.querySelector(".todo__item");
-    const newItem = template.cloneNode(true);
-    newItem.querySelector(".todo__task").textContent = item.title;
-
-    newItem
-      .querySelector(".todo__btn--functional")
-      .addEventListener("click", function (openModal) {
-        modalElem.style.visibility = "visible";
-      });
-    newItem.querySelector(".todo__checkbox");
-    list.appendChild(newItem);
-  });
+  data.forEach((item) => renderItem(item));
 };
 
 fetch("https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5")
